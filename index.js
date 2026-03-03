49e00e3c65a7c330fa7bbe98fd9d3118a97bee1a
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-03-03 19:28:50 +0000

    done1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let addition = 50; 
+addition = addition + 5;
+let subtraction = 40;
+subtraction = subtraction - 1;
+let multiplication = 5;
+multiplication = multiplication * 7;
+let division = 6;
+division = division / 2;
+document.getElementById('result').innerHTML = 
 
